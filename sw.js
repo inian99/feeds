a115cca0dbaa51978da6fda0364de9abeba0065c
inian99 <inian99@hotmail.com>
2026-08-18 18:48:33 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'feeds-v20';
+const CACHE = 'feeds-v24';
 const FILES = ['./', './index.html', './manifest.webmanifest', './apple-touch-icon.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
